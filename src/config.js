9ba3b3d26c22e4b43f3efc94424d8bf7b0d740a4
MarinaRH <l_marina1717@hotmail.com>
2018-05-19 01:23:28 -0500

    Adding config firebase

---
+++ src/config.js
@@ -0,0 +1,12 @@
+
+  // Initialize Firebase
+  export const DB_CONFIG = {
+    apiKey: "AIzaSyBL4sszMgTHl2wwl3l74jLZEjziCoqwDYE",
+    authDomain: "reactnotes-b5d10.firebaseapp.com",
+    databaseURL: "https://reactnotes-b5d10.firebaseio.com",
+    projectId: "reactnotes-b5d10",
+    storageBucket: "",
+    messagingSenderId: "228931557429"
+  };
+//   firebase.initializeApp(config);
+// npm i firebase
